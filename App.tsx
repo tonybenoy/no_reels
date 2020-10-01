65c9da92fe5fc4e7ff7e5c4d3436553258fff1a9
Tony Benoy <me@tonybenoy.com>
2020-10-01 13:19:36 +0530

    typescript

--- App.tsx
+++ App.tsx
@@ -1,21 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+import { WebView } from 'react-native-webview';
+
+
+
+export default class App extends React.Component {
+
+  render() {
+
+    return <WebView source={{ uri: 'https://instagram.com/'}} style={{ marginTop: 24 }} />;
+
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
