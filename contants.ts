b42b94f84041059f7b4363edc2d57a37c3366230
Manoranjan <manoranjan@finception.in>
2020-10-11 00:34:40 +0530

    added pull to refresh functionality in the webview via scrollview

---
+++ contants.ts
@@ -0,0 +1,9 @@
+export const INJECTED_JS_WEBVIEW = `
+  window.onscroll = function() {
+    window.ReactNativeWebView.postMessage(
+      JSON.stringify({
+        scrollTop: document.documentElement.scrollTop || document.body.scrollTop
+      }),     
+    )
+  }
+`
